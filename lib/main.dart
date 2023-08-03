f2ce51e6fa421e29f05c742f580887bcbff3f8b1
Monika Skotniczna <woszczek.monika@gmail.com>
2023-08-03 18:15:30 +0200

    Seperate log to items repository

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return 
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
